0e17289d0f9168539c9b4143a90b66839a3928b7
LorenzoLizzo <lorenzo.lizzo.stud@ispascalcomandini.it>
2021-01-05 17:18:22 +0100

    Implementato metodo di movimento delle immagini

--- Programmazione concorrente con elementi grafici di WPF/MainWindow.xaml.cs
+++ Programmazione concorrente con elementi grafici di WPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
 
 namespace Programmazione_concorrente_con_elementi_grafici_di_WPF
 {
@@ -20,9 +21,60 @@ namespace Programmazione_concorrente_con_elementi_grafici_di_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        Thread t1;
+        Thread t2;
+        Thread t3;
+
+        Random r;
+
         public MainWindow()
         {
             InitializeComponent();
+            r = new Random();
+
+            t1 = new Thread(new ThreadStart(MuoviPrimaNavicella));
+            t2 = new Thread(new ThreadStart(MuoviSecondaNavicella));
+            t3 = new Thread(new ThreadStart(MuoviTerzaNavicella));
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
         }
+
+        public void MetodoMovimento(Image img)
+        {
+            int marginTop = 0;
+            int marginLeft = 0;
+            img.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                marginTop = (int)img.Margin.Top;
+                marginLeft = (int)imgNavicella1.Margin.Left;
+            }));
+            while (marginLeft < 700)
+            {
+                Thread.Sleep(TimeSpan.FromMilliseconds(r.Next(1, 751)));
+                marginLeft += 50;
+                this.Dispatcher.BeginInvoke(new Action(() =>
+                {
+                    img.Margin = new Thickness(marginLeft, marginTop, 0, 0);
+                }));
+            }
+        }
+
+        public void MuoviPrimaNavicella()
+        {
+            MetodoMovimento(imgNavicella1);
+        }
+
+        public void MuoviSecondaNavicella()
+        {
+            MetodoMovimento(imgNavicella2);
+        }
+
+        public void MuoviTerzaNavicella()
+        {
+            MetodoMovimento(imgNavicella3);
+        }
+
     }
 }
