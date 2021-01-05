f2834158e8cd6ff94f3b29bbeb5d98cf5c6f5121
LorenzoLizzo <lorenzo.lizzo.stud@ispascalcomandini.it>
2021-01-05 16:49:21 +0100

    Impostazione mainwindow con immagini

---
+++ Programmazione concorrente con elementi grafici di WPF/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Programmazione_concorrente_con_elementi_grafici_di_WPF
+{
+    /// <summary>
+    /// Logica di interazione per MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
